8c2d36562de13dddb0440053ca23aebfb19d8803
Krishang <krishangsaharia2@gmail.com>
2020-11-04 22:57:10 +0530

    Removed Extra Lines and Added + sign validator check for phonenumber

--- users/models.py
+++ users/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-
-
-
-phone_regex = RegexValidator(regex = r'^\+?1?\d{4,15}$', message = "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+phone_regex = RegexValidator(regex = r'^\+\d{4,15}$', message = "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 otp_regex = RegexValidator(regex = r'^\d{4}')
 
-
 class Profile(models.Model):
-
     company_name = models.CharField(max_length = 200, verbose_name = "Company Name", null = True, blank = True)
     phone_number = models.CharField(max_length = 17, validators = [phone_regex], verbose_name = 'Phone No.', null = True, blank = True)
     name = models.CharField(max_length = 200, null = True, blank = True)
@@ -20,14 +15,12 @@ class Profile(models.Model):
     otp_timestamp = models.DateTimeField(auto_now = True, verbose_name = "OTP Created On")
 
     def __str__(self):
-        return str(self.phone_number)
+        return self.phone_number
 
     class Meta:
         verbose_name_plural = "Profiles"
 
-
 class Address(models.Model):
-
     profile = models.ForeignKey(to = Profile, on_delete = models.CASCADE, null = True, blank = True)
     address = models.CharField(max_length = 100, null = True, blank = True)
     city = models.CharField(max_length = 100, null = True, blank = True)
--- users/views.py
+++ users/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from .models import Profile
 from django.utils import timezone
-from .serializers import OTPSerializer
 import random
+
+from .serializers import OTPSerializer
+from .models import Profile
 
 @api_view(['POST',])
 def generate_otp(request):
-
     if request.method == "POST":
         user_profile = None
         phone_number = request.POST.get('phone_number')
@@ -23,30 +21,26 @@ def generate_otp(request):
         if not serializer.is_valid():
             return Response({'error':'Phone Number Validation Error!'}, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-        else:
-            try:
-                user_profile = Profile.objects.get(phone_number = phone_number)
-            except Profile.DoesNotExist:
-                user_profile = Profile.objects.create(phone_number = phone_number)
-            print(phone_number)
-            print(user_profile)
-            if user_profile:
-                user_profile.otp = otp
-                user_profile.save()
-                print(otp)
-                data = {'success':'OTP Generated Successfully!', 'OTP':otp}
-                return Response(data, status = status.HTTP_200_OK)
-            else:
-                return Response({'error':'User Does Not Exist'}, status = status.HTTP_404_NOT_FOUND)
+        try:
+            user_profile = Profile.objects.get(phone_number = phone_number)
+        except Profile.DoesNotExist:
+            user_profile = Profile.objects.create(phone_number = phone_number)
 
+        if user_profile:
+            user_profile.otp = otp
+            user_profile.save()
+            data = {'success':'OTP Generated Successfully!', 'OTP':otp}
+            return Response(data, status = status.HTTP_200_OK)
+        else:
+            return Response({'error':'User Does Not Exist'}, status = status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST',])
 def verify_otp(request):
-
     if request.method == "POST":
         otp = request.POST.get('otp')
         phone_number = request.POST.get('phone_number')
         serializer = OTPSerializer(data=request.data)
+
         if not serializer.is_valid():
             return Response({'error':'Phone Number Not Validated!'}, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
 
@@ -54,19 +48,15 @@ def verify_otp(request):
 
         try:
             user_profile = Profile.objects.get(phone_number = phone_number)
-
         except Profile.DoesNotExist:
-            pass
+            return Response({'error':'Please Generate OTP First!'})
 
         if user_profile:
-            if otp == str(user_profile.otp) and (timezone.now() - user_profile.otp_timestamp).seconds < 30:
-                print("SUCCESS")
-                data = {'success':'OTP Verified Successfully!', 'OTP':otp}
-                return Response(data, status = status.HTTP_200_OK)
-
+            if otp == str(user_profile.otp) :
+                if (timezone.now() - user_profile.otp_timestamp).seconds < 10:
+                    data = {'success':'OTP Verified Successfully!', 'OTP':otp}
+                    return Response(data, status = status.HTTP_200_OK)
+                else:
+                    return Response({'failure':'OTP has expired!'}, status = status.HTTP_404_NOT_FOUND)
             else:
-                print("FAILURE")
-                return Response({'failure':'OTP Verification Failed!'}, status = status.HTTP_404_NOT_FOUND)
-
-        else:
-            return Response({'error':'User Does Not Exist'}, status = status.HTTP_404_NOT_FOUND)
+                return Response({'failure':'OTP Verification Failed!. The entered OTP is incorrect'}, status = status.HTTP_404_NOT_FOUND)
