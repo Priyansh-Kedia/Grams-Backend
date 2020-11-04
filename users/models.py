3483d9db399d70bcb3a72382f911cd7c57bc8134
Krishang <krishangsaharia2@gmail.com>
2020-11-04 13:14:28 +0530

    added otp and otp_timestamp field , and made all feilds null=True and
    blank=True

--- users/models.py
+++ users/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils import timezone
 #from django_countries.fields import CountryField
+
+
+
+
+class Profile(models.Model):
+
+    otp_regex = RegexValidator(regex = r'^\d{4}')
+    phone_regex = RegexValidator(regex = r'^\+?1?\d{4,15}$', message = "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    company_name = models.CharField(max_length = 200, verbose_name = "Company Name",null = True, blank = True)
+    contact_person_details = models.CharField(max_length = 17, validators = [phone_regex], verbose_name = 'Phone No.', null = True, blank = True)
+    name = models.CharField(max_length = 200, null = True, blank = True)
+    designation = models.CharField(max_length = 100,null = True, blank = True)
+    email_id = models.EmailField(null = True,blank = True)
+    i_agree = models.BooleanField(default = False, help_text = "Please Tick If you agree to the Terms and Conditions of the Contract",null = True,blank = True)
+    otp = models.IntegerField(validators = [otp_regex])
+    otp_timestamp = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.name
 
 
 class Address(models.Model):
     #profile=models.ForeignKey(Profile,on_delete=models.CASCADE)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
+    profile = models.ForeignKey(to = Profile, on_delete = models.CASCADE, null = True, blank = True)
+    address = models.CharField(max_length = 100, null = True, blank = True)
+    city = models.CharField(max_length = 100, null = True, blank = True)
+    state = models.CharField(max_length = 100, null = True, blank = True)
+    country = models.CharField(max_length = 100, null = True, blank = True)
 
     def __str__(self):
         return self.address
-
-class Profile(models.Model):
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{4,15}$',message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    company_name = models.CharField(max_length=200,verbose_name="Company Name")
-    contact_person_details = models.CharField(max_length=17,validators=[phone_regex],verbose_name='Phone No.')
-    name = models.CharField(max_length=200)
-    address = models.ManyToManyField(Address)
-    designation = models.CharField(max_length=100)
-    email_id = models.EmailField()
-    i_agree = models.BooleanField(default=False,required=True,help_text="Please Tick If you agree to the Terms and Conditions of the Contract")
-
-    def __str__(self):
-        return self.name
 
 
 # Create your models here.
