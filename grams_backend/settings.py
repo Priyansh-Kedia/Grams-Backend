16570974719d26fedebe3c706c19d7b846f746c6
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-11-19 23:09:31 +0530

    minor changes

--- grams_backend/settings.py
+++ grams_backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'bzrkkx1@u^-l@+vi7!zgkzpan(s-c=h4vfg$z+*+*(e20@62=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['grams-testing.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
