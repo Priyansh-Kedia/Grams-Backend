8eb450fc901e7321e1e544a651d076db60ea9e6d
Krishang <krishangsaharia2@gmail.com>
2020-11-30 00:17:52 +0530

    renamed user_id to profile_id

--- process_grains/views.py
+++ process_grains/views.py
@@ -11,7 +11,7 @@ from .serializers import ScanSerializer
 @api_view(['GET',])
 def retreive_scan(request):
     if request.method == "GET":
-        user_id = request.GET['user_id']
+        user_id = request.GET['profile_id']
         scan_set = Profile.getAllScans(user_id)
         scan_serializer = ScanSerializer(scan_set, many = True)
         return Response(scan_serializer.data, status = status.HTTP_200_OK)
