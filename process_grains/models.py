7a02737bff7134c934ffc85a200906b31366243c
Krishang <krishangsaharia2@gmail.com>
2020-12-26 20:54:53 +0530

    added fields length, width, no_of_kernels,area etc in Scan Model

--- process_grains/models.py
+++ process_grains/models.py
@@ -9,6 +9,14 @@ class Scan(models.Model):
     item_type = models.CharField(max_length = 100, null = True, blank = True)
     sub_type = models.CharField(max_length = 100, null = True, blank = True)
     created_on = models.DateTimeField(auto_now_add = True)
+    no_of_particles = models.IntegerField(null = True, blank = True)
+    avg_area = models.DecimalField(max_digits = 5, decimal_places = 2, null = True, blank = True)
+    avg_length = models.DecimalField(max_digits = 5, decimal_places = 2, null = True, blank = True)
+    avg_width = models.DecimalField(max_digits = 5, decimal_places = 2, null = True, blank = True)
+    avg_l_by_w = models.DecimalField(max_digits = 5, decimal_places = 2, null = True, blank = True)
+    avg_circularity = models.DecimalField(max_digits = 5, decimal_places = 2, null = True, blank = True)
+    lot_no = models.CharField(max_length = 100, null = True, blank = True)
+    no_of_kernels = models.IntegerField(null = True, blank = True)
 
     def __str__(self):
         return str(self.id)
