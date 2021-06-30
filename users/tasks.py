b24047a049bd33886a454f13805a0649ce6257f8
Priyansh Kedia <kediapriyansh@gmail.com>
2021-07-01 01:26:38 +0530

    minor change

--- users/tasks.py
+++ users/tasks.py
@@ -49,7 +49,6 @@ def run_ml_code(phone_number,image_url,item_type,sub_type):
         requests.post(    "https://onesignal.com/api/v1/notifications",    headers={"Authorization": "Basic NDJkOGMyZDQtMjgyYi00Y2JkLWFjZTgtZGQ2NjQ1NDUwNzg3"}, json=data)
         print(ml_list)
     except Exception as e:
-        print("e ", e)
         heading_msg = "Your results are unable to be computed"
         content_msg = e
         data = { "app_id": "fad6e42a-0b02-45d6-9ab0-a654b204aca9", "contents": {"en": content_msg}, "headings": {"en": heading_msg}, "include_external_user_ids": [phone_number] , "chrome_web_image": "https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"}
