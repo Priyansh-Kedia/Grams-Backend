304672bfa0c9eff7b6c42ac5dbb3342b0020d10f
KEDIARAHUL135 <kediarahul135@gmail.com>
2021-07-08 12:12:17 +0530

    Refactor code
    Add raise exceptions code.

--- poseSegmentation.py
+++ poseSegmentation.py
@@ -9,8 +9,7 @@ import process_outlines as po
 try:
     import Compiled as cc
 except:
-    print("\nInstall cellpose dependencies first.")
-    exit()
+    raise ValueError("Install cellpose dependencies first.")
 
 
 # Parsing the arguments
@@ -74,6 +73,9 @@ def ApplyCellpose(Image, DownscaleFactor=0.25, UpscaleFactor=4, SaveOutlines=Tru
     outlinesStart = t.time()
     outlines = po.outlines_list_Fast(masks, DownscaleFactor, UpscaleFactor)
     print("Time Taken for calculating outlines: {}".format(t.time() - outlinesStart))
+
+    if outlines is None or len(outlines) == 0:
+        return []
 
     # Correcting outlines
     if CorrectOutlinesFlag:
