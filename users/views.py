2e7edb41d13bc29c8b7865182249ce2075bfd865
Priyansh Kedia <kediapriyansh@gmail.com>
2021-07-10 19:01:45 +0530

    changes in views

--- users/views.py
+++ users/views.py
@@ -84,6 +84,11 @@ def verify_otp(request):
         if user_profile:
             if otp == str(user_profile.otp) :
                 if (timezone.now() - user_profile.otp_timestamp).seconds < 1800:
+                    current,_ = CurrentStatus.objects.create(user = user_profile)   
+                    if not current.name:
+                        current.name = TrialResponse.TRIAL1
+                        current.end_date = datetime.now()+timedelta(Constants.FREETRIAL1_DAYS)
+                        current.save()
                     data = {Constants.MESSAGE:'OTP Verified Successfully!', Constants.PROFILE:model_to_dict(user_profile), Constants.IS_VERIFIED:True}
                     return Response(data, status = status.HTTP_200_OK)
                 else:
